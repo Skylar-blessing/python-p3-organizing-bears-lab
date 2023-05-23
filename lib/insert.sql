6732a664d8c249b75e6c793e1e974f5e831f0fc7
Skylar <skylarblessings5@gmail.com>
2023-05-23 06:29:40 +0300

    task

---
+++ lib/insert.sql
@@ -0,0 +1,23 @@
+INSERT INTO bears (id, name, age, sex, color, temperament, alive)
+VALUES (1, 'Mr. Chocolate', 5, 'M', 'Brown', 'Calm', 1);
+
+INSERT INTO bears (id, name, age, sex, color, temperament, alive)
+VALUES (2, 'Rowdy', 3, 'M', 'Black', 'Playful', 1);
+
+INSERT INTO bears (id, name, age, sex, color, temperament, alive)
+VALUES (3, 'Tabitha', 4, 'F', 'Brown', 'Gentle', 1);
+
+INSERT INTO bears (id, name, age, sex, color, temperament, alive)
+VALUES (4, 'Sergeant Brown', 6, 'M', 'Brown', 'Aggressive', 1);
+
+INSERT INTO bears (id, name, age, sex, color, temperament, alive)
+VALUES (5, 'Melissa', 2, 'F', 'White', 'Curious', 1);
+
+INSERT INTO bears (id, name, age, sex, color, temperament, alive)
+VALUES (6, 'Grinch', 7, 'M', 'Green', 'Grumpy', 1);
+
+INSERT INTO bears (id, name, age, sex, color, temperament, alive)
+VALUES (7, 'Wendy', 3, 'F', 'Brown', 'Shy', 1);
+
+INSERT INTO bears (id, name, age, sex, color, temperament, alive)
+VALUES (8, 'Unnamed', NULL, NULL, NULL, NULL, 1);
